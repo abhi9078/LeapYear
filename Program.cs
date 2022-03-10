dd5c4353acea99520251cbd126c21e84e363aec7
Abhilash <abhilashmeher1234@gmail.com>
2022-03-10 18:05:11 +0530

    Added all files

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Leap Year Problem ");
+            Console.WriteLine("Enter year to check: ");
+            int year=int.Parse(Console.ReadLine());
+            bool isleapyear=Program.isLeapYear(year);
+            if (isleapyear)
+            {
+                Console.WriteLine("Leap Year");
+            }
+            else
+            {
+                Console.WriteLine("Not a LeapYear");
+            }
+        }
+        public static bool isLeapYear(int year)
+        {
+            if ((year%400==0) || (year%100!=0 && year % 4 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
